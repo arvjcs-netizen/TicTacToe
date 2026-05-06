0c27f248a096bb7d6d07ded61856d96f179488ad
arvjcs-netizen <arvjcs@gmail.com>
2026-05-06 14:12:33 -0700

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,26 @@
+package TicTacToe;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Welcome to Tic Tac Toe!"); 
+        System.out.println("");
+        Board board = new Board();
+        while (true){
+            board.getMove();
+            if ((board.check().equals("none"))){
+
+            }else{
+                System.out.println("Player " + board.check() + " has won!");
+                break;
+            }
+        }
+
+        
+
+
+
+    }
+}
